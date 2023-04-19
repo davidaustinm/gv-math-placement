0447f6bbc7947e69612eb5b53a63d54af3ffb18b
David Austin <david.austin.m@gmail.com>
2023-04-19 18:06:30 -0400

    settings

--- placement/settings.py
+++ placement/settings.py
@@ -137,4 +137,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
